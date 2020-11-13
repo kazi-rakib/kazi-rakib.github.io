83e8061a3129500a00e91ffcdf9f7b9cb04ea1f0
Kazi Rakib <kmrakib168@gmail.com>
2020-11-14 02:32:31 +0600

    Update test/

---
+++ test/sass/dist/js/main.js
@@ -0,0 +1,10 @@
+// console.log("Atatched successfully!");
+
+let firstname = 'Rakib';
+let lastname = 'Hasan';
+
+let msg = `${firstname} is my first name \nand ${lastname} is my last name.`;
+
+const myCalc = amnt => amnt/100;
+
+console.log(myCalc(75));
